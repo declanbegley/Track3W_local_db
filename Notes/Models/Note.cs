55aeb096c32dd7251231654d2552c6a314f2e7ae
declan begley <declanbegley@outlook.com>
2020-05-18 14:07:34 +0100

    First save

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string AssetID { get; set; }
+        public string Location { get; set; }
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Notes/NotesPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+using Notes.Models;
+
+namespace Notes
+{
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listView.ItemsSource = await App.Database.GetNotesAsync();
+        }
+
+        async void OnNoteAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NoteEntryPage
+            {
+                BindingContext = new Note()
+            });
+        }
+
+     
+
+        public async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new NoteEntryPage
+                {
+                    BindingContext = e.SelectedItem as Note
+                });
+            }
+        }
+
+        public async void OnPostClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Post pressed", "Send the data get the ack and delete local data.", "OK");
+        }
+    }
+}
